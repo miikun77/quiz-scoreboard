b2c861fcb9faa1889089a6d8d76a2c7828b0fe24
miikun77 <64695370+miikun77@users.noreply.github.com>
2022-03-11 01:30:09 +0900

    ver1.0

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var correct = {cr1:'0', cr2:'0', cr3:'0'};
+var value = {v1:'0', v2:'0', v3:'0', v4:'0', v5:'0', v6:'0', v7:'0', v8:'0'};
 
 function minuscounter(){
     player = Math.round(event.target.id);
-    correct['cr'+player]--;
-    document.getElementById("correct"+ player).innerHTML = correct['cr'+player];
+    value['v'+player]--;
+    document.getElementById("btn"+ player).innerHTML = value['v'+player];
 }
 
 function pluscounter(){
     player = Math.round(event.target.id);
-    correct['cr'+player]++;
-    document.getElementById("correct"+ player).innerHTML = correct['cr'+player];
+    value['v'+player]++;
+    document.getElementById("btn"+ player).innerHTML = value['v'+player];
 }
